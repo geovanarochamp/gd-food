e1b30f7c4e30d18005c03ede05d28f146f40c59b
geovanarochamp <geovana.rocha.martins@gmail.com>
2022-12-24 09:19:34 -0300

    set defaultTheme

---
+++ src/styles/themes/defaultTheme.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+export const defaultTheme = {
+    white: '#FFF',
+
+    'gray-100': '#DDE2E4',
+    'gray-300': '#989B9E',
+    'gray-500': '#787878',
+    'gray-700': '#373E44',
+    'gray-900': '#252C32',
+
+    'red-300': '#FF6987',
+    'red-500': '#f6355c',
+    'red-700': '#c32a48',
+}
