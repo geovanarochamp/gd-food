c216f5a8895b7feca472ec27aeb90516adbca7ab
geovanarochamp <geovana.rocha.martins@gmail.com>
2022-12-24 09:37:38 -0300

    add globalstyles

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,9 +1,8 @@
-/* eslint-disable prettier/prettier */
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/defaultTheme'
 
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType { }
+	export interface DefaultTheme extends ThemeType {}
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/defaultTheme'
 
 function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <div>
-        <h1>GD Food</h1>
-      </div>
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<div>
+				<h1>GD Food</h1>
+			</div>
+		</ThemeProvider>
+	)
 }
 
 export default App
